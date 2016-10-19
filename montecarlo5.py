b91a6b0bd5d94de571b8c926dcf840ff5746ebe0
au-revoir <govindg0204@gmail.com>
2016-10-20 00:31:56 +0530

    Add files via upload

---
+++ montecarlo5.py
@@ -0,0 +1,71 @@
+import random
+import matplotlib
+import matplotlib.pyplot 
+def cal_dice_roll():
+    outcome=random.randint(1,100)
+    if 50<=outcome<=99:
+       # print outcome,"You won! Time to celebrate!"
+        return True
+    elif outcome==100:
+        #print outcome,"You lost! I am afraid you'll have to fork out the money from your pocket!"
+        return False
+    elif outcome<50:
+        #print outcome,"You lost! I am afraid you'll have to fork out the money from your pocket!" 
+        return False
+count=1
+def odds(tot_amt,wager):
+    amount=tot_amt
+    xCor=[]
+    yCor=[]
+    original_wager=wager
+    previous_outcome='Won'
+    for count in range(1,101):
+        
+        if previous_outcome=='Won':
+            #amount=amount+wager
+            print "Won"
+            if cal_dice_roll():
+                print "True"
+                amount=amount+wager
+                wager=original_wager
+                print amount
+                xCor.append(count)
+                yCor.append(amount)
+                #previous_outcome='Won'
+                # print amount
+            else:
+                print "False"
+                amount=amount-wager
+                wager=original_wager
+                print amount
+                previous_outcome='Lost'
+                xCor.append(count)
+                yCor.append(amount)
+                if amount <=0:
+                    break
+        elif previous_outcome=='Lost':
+             print "Lost"
+             
+             if cal_dice_roll():
+                 print "True"
+                 amount=amount+wager
+                 print amount
+                 xCor.append(count)
+                 yCor.append(amount)
+                 previous_outcome='Won'
+                 wager=original_wager
+             else:
+                 
+                 print "False"
+                 amount=amount-wager
+                 print amount
+                 xCor.append(count)
+                 yCor.append(amount)
+                 #previous_outcome='Lost'
+                 if amount<=0:
+                     break
+    matplotlib.pyplot.plot(xCor,yCor)      
+
+#for i in range(1,100):
+odds(10000,100)        
+matplotlib.pyplot.show()   
